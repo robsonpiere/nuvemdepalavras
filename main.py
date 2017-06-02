6d8c07730d9ed9a11a677457a725f7f8f5aa190d
Robson Piere <robsonpiere@outlook.com>
2017-06-02 14:00:47 -0300

    Adicionando template web

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import request
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Olá humanos! obs: Mateuzim não vale nada!'
+def index():
+  return render_template('index.html')
 
 if __name__ == '__main__':
   app.run()
