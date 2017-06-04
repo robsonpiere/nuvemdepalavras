81067212f67eb459a295d0f4fa33145f643b56b8
Robson Piere <robsonpiere@outlook.com>
2017-06-04 19:25:04 -0300

    add exemplo

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def exemplo():
+  lista = [{'text':'Mateuzim','size':40},{'text':'Nada','size':20},{'text':'Vale','size':17},{'text':'Value=0','size':10}]
+  return render_template('index.html',valores=lista)
+
+@app.route('/sobre')
+def sobre():
+  integrantes = ["Mateus Fernando","Mirlaine Ribeiro","Robson Piere","Yitzhak Andrade"]
+  return render_template('sobre.html',integrantes=integrantes)
+
 
 if __name__ == '__main__':
   app.run()
